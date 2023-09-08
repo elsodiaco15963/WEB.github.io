125bfed620a0348ef90ec623a3b0f7e7483b8919
elsodiaco15963 <66685146+elsodiaco15963@users.noreply.github.com>
2023-09-08 09:46:56 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,18 @@ function prevSlide() {
 showSlide(currentSlide);
 
 setInterval(nextSlide, 5000);
+
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    }else {
+        document.querySelector('.go-top-container').classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+});
