3fcaf79894dcb73ee7d45d2e6438b79d0a7108b1
elsodiaco15963 <66685146+elsodiaco15963@users.noreply.github.com>
2023-09-10 00:40:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -45,3 +45,22 @@ document.querySelector('.go-top-container').addEventListener('click', () => {
         behavior: 'smooth'
     })
 });
+
+function mostrarMision() {
+    document.getElementById("mensajeMision").style.display = "block";
+    document.getElementById("mensajeVision").style.display = "none";
+    document.getElementById("botonMision").disabled = true;
+    document.getElementById("botonVision").disabled = false;
+}
+
+function mostrarVision() {
+    document.getElementById("mensajeMision").style.display = "none";
+    document.getElementById("mensajeVision").style.display = "block";
+    document.getElementById("botonMision").disabled = false;
+    document.getElementById("botonVision").disabled = true;
+}
+
+mostrarMision();
+
+document.getElementById("botonMision").addEventListener("click", mostrarMision);
+document.getElementById("botonVision").addEventListener("click", mostrarVision);
